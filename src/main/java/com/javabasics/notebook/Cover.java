66007e8776d4bfcedecdd30050657be293ac739a
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-21 22:15:21 +0300

    add Cover class with fields

---
+++ src/main/java/com/javabasics/notebook/Cover.java
@@ -0,0 +1,14 @@
+package com.javabasics.notebook;
+
+import java.awt.image.BufferedImage;
+
+public class Cover {
+    private String pathToImage;
+    private BufferedImage image;
+
+    // getters
+
+    // setters
+
+    // Object class methods overrides
+}
