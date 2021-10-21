3256ba301cb55762468df0251dd5cf20bc99239d
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-21 22:06:49 +0300

    add fields to Notebook class

--- src/main/java/com/javabasics/notebook/Notebook.java
+++ src/main/java/com/javabasics/notebook/Notebook.java
@@ -1,4 +1,20 @@
 package com.javabasics.notebook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Notebook {
+    private Owner notebookOwner;
+    private Cover notebookCover;
+    private ArrayList<Note> notes;
+
+    public Notebook() {}
+
+    // getters
+
+    // setters
+
+    // Object class methods overrides
+
+    // custom methods (addNote, deleteNote etc)
 }
