d8129cb7e5948eefe37d7ff6961322504b6cea95
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-21 22:02:29 +0300

    add fields to Note class

--- src/main/java/com/javabasics/notebook/Note.java
+++ src/main/java/com/javabasics/notebook/Note.java
@@ -1,4 +1,20 @@
 package com.javabasics.notebook;
 
+import java.util.Date;
+
 public class Note {
+    private Date dateCreated;
+    private Date dateUpdated;
+    private String identifier;
+    private StringBuilder text;
+
+    public Note() {}
+
+    // getters
+
+    // setters
+
+    // Object class method overrides
+
+    // custom methods (appendToNote, deleteFromNote etc )
 }
