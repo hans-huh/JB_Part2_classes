84bddfeff80a9ae1f8bea4be958c26821c1ff59a
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-21 23:43:11 +0300

    create Student and GrouplLeader classes

---
+++ src/main/java/com/javabasics/attendancejournal/students/GroupLeader.java
@@ -0,0 +1,17 @@
+package com.javabasics.attendancejournal.students;
+
+public class GroupLeader extends Student {
+    private String emergencyPhone;
+
+    public GroupLeader(){
+        super();
+    }
+
+    // getters
+
+    // setters
+
+    // Object class methods overrides
+
+    // custom logic
+}
---
+++ src/main/java/com/javabasics/attendancejournal/students/Student.java
@@ -0,0 +1,30 @@
+package com.javabasics.attendancejournal.students;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String studentID;
+    private String email;
+    private String phone;
+
+    public Student(){
+        studentID = generateID();
+    }
+
+    private String generateID(){
+        /* some logic to generate a unique id
+        e.g.
+        return UUID.randomUUID().toString();
+         */
+        return new String();
+    }
+
+    // getters
+
+    // setters
+
+    // Object class methods overrides
+
+    // custom logic
+
+}
