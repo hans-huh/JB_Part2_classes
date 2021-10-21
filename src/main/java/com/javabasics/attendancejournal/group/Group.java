a6ae5a52a6f00870f7d817e401d4a85bac587631
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-21 23:31:14 +0300

    create class Group

---
+++ src/main/java/com/javabasics/attendancejournal/group/Group.java
@@ -0,0 +1,24 @@
+package com.javabasics.attendancejournal.group;
+
+import com.javabasics.attendancejournal.students.GroupLeader;
+import com.javabasics.attendancejournal.students.Student;
+
+import java.util.ArrayList;
+
+public class Group {
+    private int groupNumber;
+    private ArrayList<Student> students;
+    private GroupLeader groupLeader;
+
+    public Group(int groupNumber){
+        this.groupNumber = groupNumber;
+    }
+
+    // getters
+
+    // setters
+
+    // Object class methods overrides
+
+    // custom logic
+}
