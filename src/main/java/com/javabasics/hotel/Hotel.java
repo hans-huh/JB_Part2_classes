345ccb81acb123750683a3e7368fe32ecc999ae3
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-22 23:30:34 +0300

    add fields to Hotel class

--- src/main/java/com/javabasics/hotel/Hotel.java
+++ src/main/java/com/javabasics/hotel/Hotel.java
@@ -1,4 +1,12 @@
 package com.javabasics.hotel;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Hotel {
+    private String hotelName;
+    private String address;
+    private String numberOfStars;
+    private final static int NUMBER_OF_ROOMS = 20;
+    private Map<Integer, Room> rooms;
 }
