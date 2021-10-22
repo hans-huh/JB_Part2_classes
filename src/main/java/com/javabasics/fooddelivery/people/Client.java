3e2614e6ae03027c0ea260621d5c947e8dd84c8b
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-23 00:15:14 +0300

    add fields to Client class

--- src/main/java/com/javabasics/fooddelivery/people/Client.java
+++ src/main/java/com/javabasics/fooddelivery/people/Client.java
@@ -1,4 +1,13 @@
 package com.javabasics.fooddelivery.people;
 
+import java.util.ArrayList;
+
 public class Client {
+    private String clientName;
+    private String phoneNumber;
+    private String email;
+    private String defaultAddress;
+
+    private ArrayList<Order> historyOfOrders;
+    private ArrayList<Order> activeOrders;
 }
