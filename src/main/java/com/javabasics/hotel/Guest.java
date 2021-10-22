c3c94dc5fa2493efbb73e2ad1a9a346244d3f5f7
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-22 23:39:20 +0300

    create Guest class

---
+++ src/main/java/com/javabasics/hotel/Guest.java
@@ -0,0 +1,12 @@
+package com.javabasics.hotel;
+
+public class Guest {
+    private String guestID;
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    private int roomNumber;
+    private int nightsBookedByGuest;
+    private double totalCharge;
+}
