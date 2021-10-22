93a34641d1500c8e57ee24ec1e2cfc92f83bda4c
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-22 20:34:56 +0300

    create Planner class

--- src/main/java/com/javabasics/planner/Planner.java
+++ src/main/java/com/javabasics/planner/Planner.java
@@ -1,4 +1,17 @@
 package com.javabasics.planner;
 
+import java.util.ArrayList;
+
 public class Planner {
+    public Planner() {}
+
+    private ArrayList<Task> plannedTasks;
+
+    // getters
+
+    // setters
+
+    // Object class methods overrides
+
+    // custom logic
 }
