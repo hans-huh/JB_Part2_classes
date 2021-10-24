e5cde5c865ab605a23e11e0be516f5829d74c0da
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-24 23:50:22 +0300

    refactor Order class, add fields to Cart class

--- src/main/java/com/javabasics/shop/Cart.java
+++ src/main/java/com/javabasics/shop/Cart.java
@@ -1,4 +1,11 @@
 package com.javabasics.shop;
 
+import java.util.Map;
+
 public class Cart {
+    private Account account;
+    private Order order;
+    private Map<Item, Integer> itemsInCart;
+    private Double totalPriceToCharge;
+    private Double totalDiscount;
 }
--- src/main/java/com/javabasics/shop/Order.java
+++ src/main/java/com/javabasics/shop/Order.java
@@ -6,7 +6,6 @@ public class Order {
     private String orderID;
     private Account account;
     private ArrayList<Item> itemsInOrder;
-    private Double totalPriceToCharge;
     private String deliveryLocation;
     private String orderStatus;
 }
