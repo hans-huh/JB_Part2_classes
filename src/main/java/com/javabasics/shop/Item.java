bb9747c0ce8d390444b6a8af3b21e361345acaa6
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-24 23:02:51 +0300

    create Item class

---
+++ src/main/java/com/javabasics/shop/Item.java
@@ -0,0 +1,14 @@
+package com.javabasics.shop;
+
+import java.awt.Image;
+import java.util.ArrayList;
+
+public class Item {
+    private String itemID;
+    private String itemName;
+    private String itemDescription;
+    private String itemCategory;
+    private Double price;
+    private Double discount;
+    private ArrayList<Image> itemGallery;
+}
