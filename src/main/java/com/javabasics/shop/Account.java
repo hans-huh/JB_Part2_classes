22f432db7cd5c0a9683f4fa96041325575eafe39
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-24 22:42:05 +0300

    add fields to Account class

--- src/main/java/com/javabasics/shop/Account.java
+++ src/main/java/com/javabasics/shop/Account.java
@@ -1,4 +1,15 @@
 package com.javabasics.shop;
 
+import java.util.ArrayList;
+
 public class Account {
+    private String accountID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private double loyaltyDiscount;
+
+    private ArrayList<Order> orderHistory;
+    private ArrayList<Order> activeOrders;
 }
