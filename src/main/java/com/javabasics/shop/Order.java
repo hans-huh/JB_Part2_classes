92fd584ea5febbb9a6f19d6432a08e13ce78989e
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-24 23:13:56 +0300

    add fields to Order class

--- src/main/java/com/javabasics/shop/Order.java
+++ src/main/java/com/javabasics/shop/Order.java
@@ -1,4 +1,12 @@
 package com.javabasics.shop;
 
+import java.util.ArrayList;
+
 public class Order {
+    private String orderID;
+    private Account account;
+    private ArrayList<Item> itemsInOrder;
+    private Double totalPriceToCharge;
+    private String deliveryLocation;
+    private String orderStatus;
 }
