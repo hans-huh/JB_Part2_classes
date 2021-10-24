3928c377610b98971c71afcc2a4f95a549f28a1d
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-25 00:05:17 +0300

    add fields to Catalog class

--- src/main/java/com/javabasics/shop/Catalog.java
+++ src/main/java/com/javabasics/shop/Catalog.java
@@ -1,4 +1,9 @@
 package com.javabasics.shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Catalog {
+    private String description;
+    private List<Item> itemsToOffer;
 }
